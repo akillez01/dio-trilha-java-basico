b3478ac7b4e5d094eafbe8a0689e2bc312c4932e
Achilles <achilles.oliveira.souza@gmail.com.com>
2024-09-19 00:10:35 -0400

    criação conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Solicitando e capturando os dados do usuário
+        try ( // Criação do objeto Scanner para capturar os dados via terminal
+                Scanner scanner = new Scanner(System.in)) {
+            // Solicitando e capturando os dados do usuário
+            System.out.println("Por favor, digite o número da conta:");
+            int numero = scanner.nextInt();
+            scanner.nextLine();  // Consumir a quebra de linha
+            System.out.println("Por favor, digite o número da agência:");
+            String agencia = scanner.nextLine();
+            System.out.println("Por favor, digite o nome do cliente:");
+            String nomeCliente = scanner.nextLine();
+            System.out.println("Por favor, digite o saldo da conta:");
+            double saldo = scanner.nextDouble();
+            // Exibindo a mensagem final com os dados fornecidos
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                    + "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+            // Fechando o scanner
+        }
     }
 }
+
